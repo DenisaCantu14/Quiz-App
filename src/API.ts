44e54140b773cbe05911bea3a3fca867c5f681ba
DenisaCantu14 <62349123+DenisaCantu14@users.noreply.github.com>
2021-02-13 19:15:07 +0200

    Added functions and style

--- src/API.ts
+++ src/API.ts
@@ -1,3 +1,16 @@
+import {shuffleArray} from './utils'
+
+    export type Question = {
+    category : string;
+    correct_answer: string;
+    difficulty :string;
+    incorrect_answers : string[];
+    question : string;
+    type : string;
+    
+}
+
+export type QuestionState = Question & {answers :string[];};
 export enum Difficulty {
     EASY = "easy",
     MEDIUM = "medium",
@@ -5,8 +18,15 @@ export enum Difficulty {
 }
 
 export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
-    const endpoint = `https://opentdb.com/api.php!amount=${amount}&difficulty=${difficulty}&type=multiple`;
+    const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+                     //   https://opentdb.com/api.php?amount=10
     const data = await (await fetch(endpoint)).json();
-    console.log(data);
+    return data.results.map((question : Question) => ({
+       ...question, 
+       answers: shuffleArray([
+           ...question.incorrect_answers, 
+           question.correct_answer,
+        ]),
+   }));
 
-}
+};
