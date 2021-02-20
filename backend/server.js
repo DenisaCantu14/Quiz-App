804a2b1e2a66b1e41e0a36774aeca051204cb251
DenisaCantu14 <62349123+DenisaCantu14@users.noreply.github.com>
2021-02-20 15:23:51 +0200

    clean code

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,19 @@
-const mangoose = require('mangoose');
-const express = require('express');
-const cors = require('cors');
-const passport = require('passport');
-const passportaLocal = require('passporta-local'.Strategy);
-const cookieParser = require('cookie-parser');
-const bcrypt = require('bcryptjs');
-const session = require('express-session');
-const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+const passportLocal = require("passport-local").Strategy;
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcryptjs");
+const session = require("express-session");
+const bodyParser = require("body-parser");
 
 const app = express();
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors({
-    origin: 'https://localhost:3000',
+    origin: 'http://localhost:3000',
     credentials: true
 }))
 
---
+++ client/src/components/SignUp.tsx
@@ -0,0 +1,41 @@
+import React, {useState} from "react";
+import axios from "axios"
+
+function SignUp()
+{
+    const [username, setUsername] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [data, setData] = useState(null);
+    const signUp = () => {
+      axios({
+        method: "POST",
+        data: {
+          username: username,
+          email: email,
+          password: password,
+        },
+        withCredentials: true,
+        url: "http://localhost:4000/register",
+      }).then((res) => console.log(res));
+    }
+    return (
+      <div className="signup">
+       
+          <p> Welcome to</p>
+          <h1>Quiz App</h1>
+          <input type="text" name = "username" placeholder="Username" onChange = {e => setUsername(e.target.value)}></input>
+          <br />
+          <input type="email" name = "email" placeholder="email" onChange = {e => setEmail(e.target.value)}></input>
+          <br />
+          <input type="password" name = "password" placeholder="Password" onChange = {e => setPassword(e.target.value)}></input> <br />
+          <input type="password" placeholder="Confirm Password"></input> <br />
+          <button onClick={signUp} >
+            Sign Up
+          </button>
+      </div>
+    );
+  
+}
+
+export default SignUp;
